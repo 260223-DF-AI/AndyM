0446497e75bdf793c5dec92587e11370e0efcd3d
Andy Mei <qyptn123@gmail.com>
2026-03-04 13:10:21 -0600

    Data analysis Exercise: completed loading, cleaning, and adding features

---
+++ Exercises/Week2 Exercises/DataAnalysis/starter_code/analysis.py
@@ -0,0 +1,121 @@
+
+import pandas as pd
+def load_data(filepath):
+    """
+    Load the orders dataset.
+    - Parse dates correclsctly
+    - Handle missing values
+    - Return a clean DataFrame
+    """
+    # loading
+    df = pd.read_csv(filepath, parse_dates=["order_date"]) # handles dates and all missing values are nan by default (which is what I want)
+
+    return df
+
+
+
+
+def explore_data(df):
+    """
+    Print basic statistics about the dataset:
+    - Shape (rows, columns)
+    - Data types
+    - Missing value counts
+    - Basic statistics for numeric columns
+    - Date range covered
+    """
+    data_types_str = ",".join(df.iloc[0].apply(lambda x: str(type(x)))) # gets the types of all columns
+    
+
+    print(f"""
+    ------Basic Information------
+    shape: {df.shape}
+    data types: {data_types_str}
+    missing value counts: {df.isnull().sum().sum()}
+    basic stats on quant, price
+        - max price: {max(df["unit_price"])}
+        - min price: {min(df["unit_price"])}
+        - average quantity sold: {df["quantity"].mean()}
+    
+    date range: {min(df["order_date"])} - {max(df["order_date"])}
+
+""")
+    
+
+def clean_data(df):
+    """
+    Clean the dataset:
+    - Remove duplicates
+    - Fill or drop missing values (document your strategy)
+    - Standardize text columns (strip whitespace, consistent case)
+    - Add calculated columns: 'total_amount' = quantity * unit_price
+    """
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    # fill/drop missing values, my strategy is to drop an entire row if it has missing data.
+    df.dropna(inplace = True)
+
+    standardize = lambda x : x.strip().lower()
+
+    # standardize text columns : I think only category and region makes sense for this
+    df["category"] = df["category"].apply(standardize) # strips white space from category and make it all lower case
+    df["region"] = df["region"].apply(standardize)
+
+    return df
+
+
+def add_time_features(df):
+    """
+    Add time-based features:
+    - day_of_week (0=Monday, 6=Sunday)
+    - month
+    - quarter
+    - is_weekend (boolean)
+    """
+    dates = df["order_date"]
+
+    # get which day it is based off the info
+    day_dictionary = {0: "Monday", 1:"Tuesday", 2:"Wednesday", 3:"Thursday", 4:"Friday", 5:"Saturday", 6:"Sunday"}
+    #print("date:",dates.dt.day, "type:", type(dates.dt.day))
+    day_of_the_week = dates.dt.day.apply(lambda x : day_dictionary[x%7])
+    #print("Day of the week:", day_of_the_week)
+    #print(df)
+
+    # get the month
+    month = dates.dt.month
+
+    # which quarter is it in
+    quarter = month.apply(lambda x: x % 4)
+
+    # check if day is sunday or saturday
+    is_weekend = []
+    for day in day_of_the_week:
+        if day == "Saturday" or day == "Sunday":
+            is_weekend.append(True)
+        else:
+            is_weekend.append(False)
+
+    # adding features to the df
+    df["day"] = day_of_the_week
+    df["month"] = month
+    df["quarter"] = quarter
+    df["weekend"] = is_weekend
+    return df
+
+
+
+if __name__ == "__main__":
+    df = load_data("orders.csv")
+    explore_data(df)
+
+    df2 = load_data("malformed_order.csv")
+    explore_data(df2)
+
+    #print("before:", df2)
+    #print("\n\nAfter:",clean_data(df2))
+    df2 = clean_data(df2)
+    
+    df3 = add_time_features(df2)
+    print("TESTING time features\n\n\n",df3)
+    
