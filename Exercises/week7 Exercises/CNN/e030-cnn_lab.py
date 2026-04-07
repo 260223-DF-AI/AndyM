cb9c3d351bd0400981468eb3c7c52d5867d38525
Andy Mei <qyptn123@gmail.com>
2026-04-07 11:49:16 -0500

    cnn lab done

--- Exercises/week7 Exercises/CNN/e030-cnn_lab.py
+++ Exercises/week7 Exercises/CNN/e030-cnn_lab.py
@@ -10,14 +10,14 @@ class PetClassifierCNN(nn.Module):
         
         # 1. First Convolutional Block
         # TODO: 3 in_channels, 16 out_channels, 3x3 kernel, padding 1
-        self.conv1 = None 
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
         self.relu1 = nn.ReLU()
         # TODO: MaxPool 2x2 kernel, stride 2
-        self.pool1 = None 
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         
         # 2. Second Convolutional Block
         # TODO: 16 in_channels, 32 out_channels, 3x3 kernel, padding 1
-        self.conv2 = None 
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1) 
         self.relu2 = nn.ReLU()
         # You can reuse the pool layer or define a new one
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -30,19 +30,19 @@ class PetClassifierCNN(nn.Module):
         flattened_size = 32 * 16 * 16
         
         # TODO: Linear layer from flattened_size to 2 output classes
-        self.fc = None
+        self.fc = nn.Linear(flattened_size, 2)
 
     def forward(self, x):
         # TODO: Route x through conv1, relu1, and pool1
-        
+        x = self.pool1(self.relu1(self.conv1(x)))
         # TODO: Route through conv2, relu2, and pool2
-        
+        x = self.pool2(self.relu2(self.conv2(x)))
         # Flatten the spatial dimensions into a 1D vector for the Linear layer
         # Output of pool2 is [Batch, 32, 16, 16]. We want [Batch, 32*16*16]
         x = x.view(x.size(0), -1) 
         
         # TODO: Route through the final fully connected (fc) layer
-        
+        x = self.fc(x)
         return x
 
 def test_forward_pass():
@@ -53,7 +53,7 @@ def test_forward_pass():
     
     # 1. Instantiate the model
     # TODO
-    model = None
+    model = PetClassifierCNN()
     
     # 2. Create a dummy image batch (4 images, 3 channels, 64x64 size)
     dummy_input = torch.randn(4, 3, 64, 64)
@@ -61,7 +61,7 @@ def test_forward_pass():
     
     # 3. Pass through the model
     # TODO
-    output = None
+    output = model(dummy_input)
     
     # 4. Print the final shape
     if output is not None:
